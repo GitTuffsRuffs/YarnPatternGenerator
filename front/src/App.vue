e1ec00a35f444c5d72dd205e0e32c622853a2528
tuffsruffs <tuffsruffs@gmail.com>
2019-12-16 20:43:21 +0100

    Component Menu

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,14 +3,7 @@
         <YearnHeader/>
 
         <div id="ContentAll">
-            <ul id="SideMenu">
-                <li>By Random</li>
-                <li>By Patterns</li>
-                <li>By Forms</li>
-                <li>By Import</li>
-                <li>By Temperature</li>
-                <li>Free Paint</li>
-            </ul>
+            <YarnMenu/>
 
             <router-view/>
         </div>
@@ -34,51 +27,6 @@
         display: flex;
         flex-direction: row;
         flex-grow: 1;
-    }
-
-    #SideMenu {
-        margin: 0;
-        padding: 0;
-        padding-top: 45px;
-        list-style: none;
-        font-size: 22px;
-        font-family: 'Merienda', cursive;
-    }
-
-    #SideMenu > li {
-        margin-bottom: 10px;
-        padding: 10px;
-        padding-right: 70px;
-        clip-path: polygon(0 0, calc(100% - 40px) 0, calc(100% - 60px) 100%, 0 100%);
-        transition: clip-path .5s;
-    }
-
-    #SideMenu > li:hover {
-        clip-path: polygon(0 0, 100% 0, calc(100% - 20px) 100%, 0 100%);
-    }
-
-    #SideMenu > li:nth-child(1) {
-        background-color: #df87ff;
-    }
-
-    #SideMenu > li:nth-child(2) {
-        background-color: #6fa2e8;
-    }
-
-    #SideMenu > li:nth-child(3) {
-        background-color: #61ff8a;
-    }
-
-    #SideMenu > li:nth-child(4) {
-        background-color: #EBE480;
-    }
-
-    #SideMenu > li:nth-child(5) {
-        background-color: #FFAE73;
-    }
-
-    #SideMenu > li:nth-child(6) {
-        background-color: #FF796C;
     }
 
     main {
@@ -114,8 +62,9 @@
 </style>
 <script>
     import YearnHeader from "./components/YearnHeader";
+    import YarnMenu from "./YarnMenu";
 
     export default {
-        components: {YearnHeader}
+        components: {YarnMenu, YearnHeader}
     }
 </script>
---
+++ front/src/YarnMenu.vue
@@ -0,0 +1,63 @@
+<template>
+    <ul id="SideMenu">
+        <li>By Random</li>
+        <li>By Patterns</li>
+        <li>By Forms</li>
+        <li>By Import</li>
+        <li>By Temperature</li>
+        <li>Free Paint</li>
+    </ul>
+</template>
+<script>
+    export default {
+        name: 'YarnMenu'
+    }
+</script>
+<style lang="less">
+
+    #SideMenu {
+        margin: 0;
+        padding: 0;
+        padding-top: 45px;
+        list-style: none;
+        font-size: 22px;
+        font-family: 'Merienda', cursive;
+
+        > li {
+            margin-bottom: 10px;
+            padding: 10px;
+            padding-right: 70px;
+            clip-path: polygon(0 0, calc(100% - 40px) 0, calc(100% - 60px) 100%, 0 100%);
+            transition: clip-path .5s;
+
+            &:hover {
+                clip-path: polygon(0 0, 100% 0, calc(100% - 20px) 100%, 0 100%);
+            }
+
+            &:nth-child(1) {
+                background-color: #df87ff;
+            }
+
+            &:nth-child(2) {
+                background-color: #6fa2e8;
+            }
+
+            &:nth-child(3) {
+                background-color: #61ff8a;
+            }
+
+            &:nth-child(4) {
+                background-color: #EBE480;
+            }
+
+            &:nth-child(5) {
+                background-color: #FFAE73;
+            }
+
+            &:nth-child(6) {
+                background-color: #FF796C;
+            }
+        }
+    }
+
+</style>
