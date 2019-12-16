1e69bac7e7264014b5166f7da2df8e95d95f0e4c
tuffsruffs <tuffsruffs@gmail.com>
2019-12-16 20:19:16 +0100

    Front: draft 1

---
+++ front/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <main id="Main">
+    <div id="Settings">1.Settings</div>
+    <div id="Grid">2.Grid</div>
+    <div id="Color">3.Colors</div>
+  </main>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home"
+};
+</script>
