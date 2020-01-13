fc2040e6c7002003c8d3b1f9a1c5c5b1f1ec3cd6
tuffsruffs <tuffsruffs@gmail.com>
2020-01-13 20:48:12 +0100

    Adding patterns

--- front/src/views/Patterns.vue
+++ front/src/views/Patterns.vue
@@ -18,7 +18,44 @@
           <button class="PatternBox" type="button" value="Imgage2">
             <img src="../assets/img/patterns/pattern2.png"/>
           </button>
+
+          <button class="PatternBox" type="button" value="Imgage3">
+            <img src="../assets/img/patterns/pattern3.png"/>
+          </button>
+
+          <button class="PatternBox" type="button" value="Imgage4">
+            <img src="../assets/img/patterns/pattern4.png"/>
+          </button>
         </div>
+
+        <div>
+          <button class="PatternBox" type="button" value="Imgage5">
+            <img src="../assets/img/patterns/pattern5.png"/>
+          </button>
+
+          <button class="PatternBox" type="button" value="Imgage6">
+            <img src="../assets/img/patterns/pattern6.png"/>
+          </button>
+
+          <button class="PatternBox" type="button" value="Imgage7">
+            <img src="../assets/img/patterns/pattern7.png"/>
+          </button>
+
+          <button class="PatternBox" type="button" value="Imgage8">
+            <img src="../assets/img/patterns/pattern8.png"/>
+          </button>
+        </div>
+
+        <div>
+          <button class="PatternBox" type="button" value="Imgage9">
+            <img src="../assets/img/patterns/pattern9.png"/>
+          </button>
+
+          <button class="PatternBox" type="button" value="Imgage10">
+            <img src="../assets/img/patterns/pattern10.png"/>
+          </button>
+        </div>
+
       </div>
 
       <!--
